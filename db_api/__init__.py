2d72c3aea62c994a6303e270952327e6e4319b60
Juan Romero <juan.romero@intelematics.com>
2021-03-31 16:26:29 +1100

    first cut

---
+++ db_api/__init__.py
@@ -0,0 +1,50 @@
+import pathlib
+from databricks_cli.workspace.api import WorkspaceApi 
+from databricks_cli.sdk import ApiClient
+
+class DBApi(object):
+
+    def __init__(
+        self,
+        token=None,
+        host='https://intelematics-dac-dev.cloud.databricks.com',
+        apiVersion='2.0',
+    ):
+        if token is None:
+            token = dbutils.secrets.get(scope="api", key="token")
+        self._client = client=ApiClient(
+                            host=host,
+                            apiVersion=apiVersion,
+                            token=token
+                            )
+
+    def export_notebook(self, source_path, target_path, fmt='DBC', is_overwrite=False):
+        (
+            WorkspaceApi(self._client)
+            .export_workspace(
+                source_path, 
+                target_path,
+                fmt,
+                is_overwrite=False
+            )
+        )
+
+    def import_notebook(self, source_path, target_path, language='PYTHON', fmt='DBC', is_overwrite=False):
+        (
+            WorkspaceApi(self._client)
+            .import_workspace(
+                source_path,
+                target_path,
+                language,
+                fmt,
+                is_overwrite
+            )
+        )
+
+    def backup_notebook(self, source_path, target_path, tmp_dir='/dbfs/mnt/external/tmp/'):
+        notebook_name = pathlib.Path(source_path).parts[-1]
+        intermediate_location = pathlib.Path(tmp_dir).joinpath(notebook_name).as_posix()
+        self.export_notebook(source_path, intermediate_location)
+        self.import_notebook(intermediate_location, target_path)
+
+
