5fd94a5e390c9c0ff2061b882196e76ca90479fb
Leonard Li <limeng0525@gmail.com>
2021-04-26 19:57:12 +1000

    make log more readable

--- bricklayer/crawler/__init__.py
+++ bricklayer/crawler/__init__.py
@@ -54,14 +54,22 @@ class Crawler():
             if not path_list:
                 logging.warn(f'Cannot find any qualified path in {working_dir_abs_path}')
                 return
+
+            success_paths = []
+            failure_paths = []
             for p in path_list:
                 table_name = p.relative_to(working_dir_abs_path).parts[0]
                 version = p.relative_to(working_dir_abs_path).parts[1].split('version=')[1]
                 table_sql_name = f'{table_name}_version_{version}'
                 table_location_path = f'/{working_dir}/{table_name}/version={version}'
-                self._create_delta_table(table_sql_name, table_location_path)
+                if self._create_delta_table(table_sql_name, table_location_path):
+                    success_paths.append(p)
+                else:
+                    failure_paths.append(p)
+            logging.info(f"Restoring successful: {success_paths}")
+            logging.info(f"Restoring failed: {failure_paths}")
 
-    def _create_delta_table(self, table_sql_name: str, table_location_path: str) -> None:
+    def _create_delta_table(self, table_sql_name: str, table_location_path: str) -> bool:
         sql = f"""
         CREATE TABLE {table_sql_name}
         USING DELTA 
@@ -71,9 +79,11 @@ class Crawler():
         if Path(f'/dbfs{table_location_path}/_delta_log').exists():
             self.spark.sql(sql)
             logging.info(f'Restoring delta table for {table_sql_name} at {table_location_path} SUCCESS')
+            return True
         else:
-            logging.error(f'`/dbfs{table_location_path}/_delta_log` doesn\'t exist')
-            logging.error(f'Restoring delta table for {table_sql_name} at {table_location_path} FAILED')
+            logging.debug(f'`/dbfs{table_location_path}/_delta_log` doesn\'t exist')
+            logging.debug(f'Restoring delta table for {table_sql_name} at {table_location_path} FAILED')
+            return False
 
     def relocate_delta_tables(self, working_dir: str, tables: typing.Iterable[str] = None) -> None:
         """update the location url for all tables which could be retrieved by Databricks sql
@@ -95,12 +105,19 @@ class Crawler():
             logging.warn(f'Cannot find any qualified table to relocate')
             return
 
+        success_tables = []
+        failure_tables = []
         for t in tables:
             table_name, version = t.split('_version_')
             table_new_location_path = f'/{working_dir}/{table_name}/version={version}'
-            self._update_delta_table_location(t, table_new_location_path)
+            if self._update_delta_table_location(t, table_new_location_path):
+                success_tables.append(t)
+            else:
+                failure_tables.append(t)
+        logging.info(f"Relocating successful: {success_tables}")
+        logging.info(f"Relocating failed: {failure_tables}")
 
-    def _update_delta_table_location(self, table_sql_name: str, table_new_location_path: str) -> None:
+    def _update_delta_table_location(self, table_sql_name: str, table_new_location_path: str) -> bool:
         sql = f"""
         ALTER TABLE {table_sql_name}
         SET LOCATION '{table_new_location_path}'
@@ -109,9 +126,11 @@ class Crawler():
         if Path(f'/dbfs{table_new_location_path}/_delta_log').exists():
             self.spark.sql(sql)
             logging.info(f'Relocating delta table for {table_sql_name} to {table_new_location_path} SUCCESS')
+            return True
         else:
-            logging.error(f'`/dbfs{table_new_location_path}/_delta_log` doesn\'t exist')
-            logging.error(f'Relocating delta table for {table_sql_name} to {table_new_location_path} FAILED')
+            logging.debug(f'`/dbfs{table_new_location_path}/_delta_log` doesn\'t exist')
+            logging.debug(f'Relocating delta table for {table_sql_name} to {table_new_location_path} FAILED')
+            return False
 
     def _get_all_tables(self):
         return [f'{db}.{table}'
