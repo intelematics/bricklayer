dbbbd3f7d35818d229d2752c3b661f12da6e16a8
Leonard Li <limeng0525@gmail.com>
2021-04-27 13:51:44 +1000

    response to PR review

--- bricklayer/crawler/__init__.py
+++ bricklayer/crawler/__init__.py
@@ -5,63 +5,66 @@
     - update existing delta table from delta_log location
     Usage:
     ```
-    from bricklayer.crawler import Crawler
+    import bricklayer.crawler
     # restore tables
-    c = Crawler()
     c.restore_delta_tables(
-        working_dir='/mnt/dev_data_asset/delta',
-        tables= ['flow.feature_flow_intelemap_link_version_1']
+        dbfs_path='/mnt/dev_data_asset/delta',
+        table_names= ['flow.feature_flow_intelemap_link_version_1']
     )
     # relocate tables
     c.relocate_delta_tables(
-        working_dir='/mnt/dev_data_asset/delta',
-        tables= ['flow.feature_flow_intelemap_link_version_1']
+        dbfs_path='/mnt/dev_data_asset/delta',
+        table_names= ['flow.feature_flow_intelemap_link_version_1']
     )
     ```
 """
 
-from pathlib import Path
 import typing
 import logging
+from pathlib import Path
 from pyspark.sql import SparkSession
+from ..catalog.dbricks_catalog import DbricksDatabase, DbricksCatalog
 
 class Crawler():
 
     def __init__(self):
         self.spark = SparkSession.builder.getOrCreate()
 
-    def restore_delta_tables(self, working_dir: str, tables: typing.Iterable[str] = None) -> None:
+    def restore_delta_tables(self, dbfs_path: str, table_names: typing.Iterable[str] = None) -> None:
         """recreate delta tables for all delta_log/ path which was found in the target directory
         Args:
-            working_dir (str): working directory in which save the delta table data
+            dbfs_path (str): relative path to dbfs/ in which save the delta table data
                 e.g. '/mnt/dev_data_asset/delta'
             tables (typing.Iterable[str], optional): tables(table_sql_name) to be restored
                 e.g. flow.feature_flow_intelemap_link_version_1
                 Defaults to None.
         """
-        logging.info(f'Input `working_dir`: {working_dir}')
-        working_dir = working_dir.strip('/')
-        working_dir_abs_path = Path(f'/dbfs/{working_dir}')
-        logging.info(f'Absolute path of the working_dir: {str(working_dir_abs_path)}')
+        if isinstance(table_names, str):
+            table_names = [table_names]
 
-        if tables:
-            for t in tables:
+        logging.info(f'Input `dbfs_path`: {dbfs_path}')
+        dbfs_path = dbfs_path.strip('/')
+        abs_path = Path(f'/dbfs/{dbfs_path}')
+        logging.info(f'Absolute full path of the directory: {str(abs_path)}')
+
+        if table_names:
+            for t in table_names:
                 table_name, version = t.split('_version_')
-                table_location_path = f'/{working_dir}/{table_name}/version={version}'
+                table_location_path = f'/{dbfs_path}/{table_name}/version={version}'
                 self._create_delta_table(t, table_location_path)
         else:
-            path_list = working_dir_abs_path.glob('*.*/version=*/_delta_log/')
+            path_list = abs_path.glob('*.*/version=*/_delta_log/')
             if not path_list:
-                logging.warn(f'Cannot find any qualified path in {working_dir_abs_path}')
+                logging.warn(f'Cannot find any qualified path in {abs_path}')
                 return
 
             success_paths = []
             failure_paths = []
             for p in path_list:
-                table_name = p.relative_to(working_dir_abs_path).parts[0]
-                version = p.relative_to(working_dir_abs_path).parts[1].split('version=')[1]
+                table_name = p.relative_to(abs_path).parts[0]
+                version = p.relative_to(abs_path).parts[1].split('version=')[1]
                 table_sql_name = f'{table_name}_version_{version}'
-                table_location_path = f'/{working_dir}/{table_name}/version={version}'
+                table_location_path = f'/{dbfs_path}/{table_name}/version={version}'
                 if self._create_delta_table(table_sql_name, table_location_path):
                     success_paths.append(p)
                 else:
@@ -85,31 +88,33 @@ class Crawler():
             logging.debug(f'Restoring delta table for {table_sql_name} at {table_location_path} FAILED')
             return False
 
-    def relocate_delta_tables(self, working_dir: str, tables: typing.Iterable[str] = None) -> None:
+    def relocate_delta_tables(self, dbfs_path: str, table_names: typing.Iterable[str] = None) -> None:
         """update the location url for all tables which could be retrieved by Databricks sql
         Args:
-            working_dir (str): working directory in which save the delta table data
+            dbfs_path (str): working directory in which save the delta table data
                 e.g. '/mnt/dev_data_asset/delta'
-            tables (typing.Iterable[str], optional): tables to be relocated
+            table_names (typing.Iterable[str], optional): tables to be relocated
                 e.g. flow.feature_flow_intelemap_link_version_1
                 Defaults to None.
         """
+        if isinstance(table_names, str):
+            table_names = [table_names]
 
-        logging.info(f'Input `working_dir`: {working_dir}')
-        working_dir = working_dir.strip('/')
+        logging.info(f'Input `dbfs_path`: {dbfs_path}')
+        dbfs_path = dbfs_path.strip('/')
 
-        if not tables:
-            tables = self._get_all_tables()
+        if not table_names:
+            table_names = self._get_all_tables()
 
-        if not tables:
+        if not table_names:
             logging.warn(f'Cannot find any qualified table to relocate')
             return
 
         success_tables = []
         failure_tables = []
-        for t in tables:
+        for t in table_names:
             table_name, version = t.split('_version_')
-            table_new_location_path = f'/{working_dir}/{table_name}/version={version}'
+            table_new_location_path = f'/{dbfs_path}/{table_name}/version={version}'
             if self._update_delta_table_location(t, table_new_location_path):
                 success_tables.append(t)
             else:
@@ -133,7 +138,15 @@ class Crawler():
             return False
 
     def _get_all_tables(self):
-        return [f'{db}.{table}'
-            for db in list(self.spark.sql("SHOW DATABASES").toPandas().databaseName)
-            for table in list(self.spark.sql(f"SHOW TABLES FROM {db} LIKE '*_version_*'").toPandas().tableName)
+        return [
+            table.sql_name
+            for db in DbricksCatalog().get_databases()
+            for table in db.get_tables()
         ]
+
+
+def restore_delta_tables(dbfs_path: str, table_names: typing.Iterable[str] = None):
+    Crawler().restore_delta_tables(dbfs_path, table_names)
+
+def relocate_delta_tables(dbfs_path: str, table_names: typing.Iterable[str] = None):
+    Crawler().relocate_delta_tables(dbfs_path, table_names)
