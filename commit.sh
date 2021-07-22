cbf6c260571271fad131113023b29005e9a7e4f9
github action <stephanie.mak@intelematics.com>
2021-07-22 16:57:05 +1000

    add -f

--- commit.sh
+++ commit.sh
@@ -6,7 +6,7 @@ git config --global user.name 'github action'
 
 if [ -n "$(git status --porcelain)" ]; then
 	echo "there are changes";
-	git add dist/*.whl
+	git add -f dist/*.whl
 	git commit -m 'publish wheel by github action'
 	git push
 else
