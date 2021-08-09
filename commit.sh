5eeb5f9e66b56f035a00083da6d766e96d56e2b2
stephanie-mak <stephanie.mak@intelematics.com>
2021-08-09 15:23:22 +1000

    Update commit.sh
    Co-authored-by: mao-liu <43122580+mao-liu@users.noreply.github.com>

--- commit.sh
+++ commit.sh
@@ -3,7 +3,10 @@
 set -ex
 
 git config --global user.name 'github action'
-
+if [[ $(git log -n 1 --format=format:%an) == "github action" ]]; then
+    echo "previous commit is by github action, skipping commit"
+    exit 0
+fi
 if [ -n "$(git status --porcelain)" ]; then
 	echo "there are changes";
 	git add package/*
@@ -12,4 +15,3 @@ if [ -n "$(git status --porcelain)" ]; then
 else
 	echo "no changes";
 fi
-
