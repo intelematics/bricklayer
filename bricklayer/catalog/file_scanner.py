35a6b1713ffa3d355be17bc91193f768a74abbbd
loganwang007 <logan.wang@intelematics.com>
2021-12-16 18:00:35 +1100

    remove hardcoded workers param

--- bricklayer/catalog/file_scanner.py
+++ bricklayer/catalog/file_scanner.py
@@ -1,13 +1,14 @@
 """
     Module to distribute the S3 download over a spark cluster
+    Useful when the data is highly partitioned and unable to be loaded by standard methods
     Results end up in a table
     Usage:
     ```
     from file_scanner import DbricksFileScanner
     # define the aws_bucket and output_dir for the s3_fetch to start
-    aws_bucket = "dev-trips-service-trips-intelematics"
+    aws_bucket = "service-trips"
     output_dir = "/tmp/"
-    # define the target df awaiting to be parse the path 
+    # define the target df awaiting to be parse the path
     df = Spark.createDataFrame()
     # export the fetched contents dataframe
     output_df = DbricksFileScanner.download_file(df, aws_bucket, output_dir, path_column)
@@ -17,6 +18,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from functools import partial
 from pyspark.sql.functions import pandas_udf, PandasUDFType
 from pyspark.sql import DataFrame
+import logging
 import os
 import boto3
 import csv
@@ -24,15 +26,15 @@ import csv
 
 class DbricksFileScanner:
     @staticmethod
-    def download_file(df: DataFrame, aws_bucket: str, output_dir: str, path_column: str):
+    def download_file(df: DataFrame, aws_bucket: str, output_dir: str, path_column: str, max_workers: int = 32):
         """encapsulate the pandas udf function as a static method
 
         Args:
             df (DataFrame): target dataframe
             aws_bucket (str): aws bucket stored all the small files
             output_dir (str): temporary output dir
-            column_path (str): path column in the target dataframe
-
+            path_column (str): path column in the target dataframe
+            max_workers (int): number of processors
         Returns:
             [DataFrame]: [output dataframe with downloaded content]
         """
@@ -62,7 +64,7 @@ class DbricksFileScanner:
             # List for storing possible failed downloads to retry later
             failed_downloads = []
 
-            with ThreadPoolExecutor(max_workers=32) as executor:
+            with ThreadPoolExecutor(max_workers) as executor:
                 # Using a dict for preserving the downloaded file for each future
                 # to store it as a failure if we need that
                 futures = {
@@ -82,8 +84,10 @@ class DbricksFileScanner:
             def read_file_and_return_contents(path):
                 try:
                     with open(output_dir + path.replace('/', '_'), 'r') as file:
+                        logging.info(f"Read {file} and return its value")
                         return file.read()
                 except FileNotFoundError:
+                    logging.warning("Messages is failed to download from s3")
                     return None
 
             return paths.apply(read_file_and_return_contents)
